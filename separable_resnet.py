7f01f020b656c1c31e95f787303cf412f389004d
Pierandrea Sartori <pierandrea.sartori@gmail.com>
2022-02-09 16:08:37 +0100

    Refactor SeparableResNet

--- separable_resnet.py
+++ separable_resnet.py
@@ -1,5 +1,4 @@
 from torch import nn
-from torch.nn import functional as F
 
 
 
@@ -16,40 +15,24 @@ class Conv2dNorm(nn.Module):
         return self.layer(x)
 
 
-class TransitionLayer(nn.Module):
-    def __init__(self, in_ch, out_ch) -> None:
+
+class SeparableConv(nn.Module):
+    def __init__(self, in_ch, out_ch, kernel_size, stride=1) -> None:
         super().__init__()
-        self.transition = nn.Sequential(
-            Conv2dNorm(in_ch, in_ch, kernel_size=5, stride=2, padding=2, groups=in_ch),
+        self.layer = nn.Sequential(
+            Conv2dNorm(in_ch, in_ch, kernel_size, stride, padding=kernel_size//2, groups=in_ch),
             Conv2dNorm(in_ch, out_ch)
         )
     
     def forward(self, x):
-        return self.transition(x)
-
-
-
-class TransitionLayerDouble(nn.Module):
-    def __init__(self, in_ch, out_ch) -> None:
-        super().__init__()
-        self.strict = Conv2dNorm(in_ch, out_ch)
-        self.large = nn.Sequential(
-            Conv2dNorm(in_ch, in_ch, kernel_size=5, stride=2, padding=2, groups=in_ch),
-            Conv2dNorm(in_ch, out_ch)
-        )
-    
-    def forward(self, x):
-        return self.strict(F.interpolate(x, scale_factor=0.5, mode='bilinear')) + self.large(x)
+        return self.layer(x)
 
 
 
 class ResBlock(nn.Module):
-    def __init__(self, channels) -> None:
+    def __init__(self, channels, kernel_size) -> None:
         super().__init__()
-        self.residual = nn.Sequential(
-            Conv2dNorm(channels, channels, kernel_size=5, padding=2, groups=channels),
-            Conv2dNorm(channels, channels)
-        )
+        self.residual = SeparableConv(channels, channels, kernel_size)
     
     def forward(self, x):
         return x + self.residual(x)
@@ -57,9 +40,9 @@ class ResBlock(nn.Module):
 
 
 class Stage(nn.Module):
-    def __init__(self, channels, repeat) -> None:
+    def __init__(self, channels, kernel_size, repeat) -> None:
         super().__init__()
-        modules = [ResBlock(channels) for _ in range(repeat)]
+        modules = [ResBlock(channels, kernel_size) for _ in range(repeat)]
         self.stage = nn.Sequential(*modules)
     
     def forward(self, x):
@@ -68,16 +51,16 @@ class Stage(nn.Module):
 
 
 class SeparableResNet(nn.Module):
-    def __init__(self, num_classes, width_factor=1, depth_factor=1):
+    def __init__(self, num_classes, kernel_size=5, width_factor=1, depth_factor=1):
         super().__init__()
         self.net = nn.Sequential(
-            Conv2dNorm(3, 16*width_factor),
-            Stage(16*width_factor, repeat=int(3*depth_factor)),
-            TransitionLayer(16*width_factor, 32*width_factor),
-            Stage(32*width_factor, repeat=int(7*depth_factor)),
-            TransitionLayer(32*width_factor, 64*width_factor),
-            Stage(64*width_factor, repeat=int(5*depth_factor)),
-            nn.AvgPool2d(8),
+            Conv2dNorm(3, 16*width_factor, kernel_size=1),
+            Stage(16*width_factor, kernel_size, repeat=(1*depth_factor)),
+            SeparableConv(16*width_factor, 32*width_factor, kernel_size, stride=2),
+            Stage(32*width_factor, kernel_size, repeat=(3*depth_factor)),
+            SeparableConv(32*width_factor, 64*width_factor, kernel_size, stride=2),
+            Stage(64*width_factor, kernel_size, repeat=(1*depth_factor)),
+            nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
             nn.Linear(64*width_factor, num_classes)
         )
